43b9d0f1e2c923263f04c50785d0fe6fb2f719ac
lucav22 <luca.villegas@nyu.edu>
2024-01-29 16:44:47 -0500

    ds

--- ds/ds.lectures/lecture 3.py
+++ ds/ds.lectures/lecture 3.py
@@ -60,6 +60,69 @@ s_iter = iter(s)
 again the iterator objects is created
 
 
+an iterator is an objects that contains or exposes a series of values that makes subsequent calls to next(iterator)
+
+
+next(lst_iter)
+1
+next(lst_iter)
+2
+
+going through the values in the collection in order as being called in next
+
+next(lst_iter)
+StopIteration error that happens when the collection is empty
+
+can use the iter function multiple times to make two different iter objects that each have their own order of nest
+
+lst = [1,2,3]
+lst_iter1 = iter(lst)
+lst_iter2 = iter(lst)
+
+s = 'abc'
+for item in s:
+    print(item)
+
+restricted on using a for loop
+
+s = 'abc'
+s_iter = iter(s)
+end = False
+make an iter object to then use a while loop to use next
+
+while(end == False ):
+    try:
+        item = next(s_iter)
+        print(item)
+    except StopIteration
+        end == True
+
+
+assign each value that is being called to item
+before the loop make sure to have a boolean variable that way when the except error is raised, the variable can be changed
+to stop the while loop since the value has been change to true
+
+for elem in range(3,10,0.5)
+    print(elem)
+
+    starts from 3 to 10 in jumps of .5 to 9.5 since its exclusive
+
+write our own version of range
+
+def my_range (start,stop, step):
+    res = []
+    curr = start
+    while (curr < stop):
+        res.append(curr)
+        curr += step
+    return res
+
+adds all values of the step into a giant lst and continues the while loop until it reaches the end
+
+
+
+
+
 
 
 ;
