31afa3fcd3b1c9453a88b991307495458e2dd59f
lucav22 <luca.villegas@nyu.edu>
2024-02-05 19:49:49 -0500

    ds

--- ds/ds.lectures/lecture 5.py
+++ ds/ds.lectures/lecture 5.py
@@ -3,7 +3,7 @@
 version II the same function can be ran except the range will be (1,num/2 +1) so it now stops at the midpoint
 and takes the midpoint as well
 
-version III takes the sqaure root of the number as a potential factor
+version III takes the square root of the number as a potential factor
 
 proof by contradiction shows that num > num and since that is a contradiction,
 in each complementary pair at least is less than or equal to square root of num
@@ -19,8 +19,10 @@ however, any primitive operation such as +,-,x,=,sqrt is constant time
 instead of taking the number of primitive operation, we take the growth of the number of operation
 this is asymptotic analysis instead of runtime analysis
 
- after finding the runtime analysis, you can ignore the number values associated to it and instead look at the
- constant that is occurring due to the input
+after finding the runtime analysis, you can ignore the number values associated to it and instead look at the
+constant that is occurring due to the input
+
+talking more about asymptotic analysis next time
 
 
 '''
