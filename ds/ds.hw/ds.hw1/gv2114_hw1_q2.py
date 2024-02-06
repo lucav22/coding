e26bd4c859d95305f5576e136ecaf1b6fd02bb24
lucav22 <luca.villegas@nyu.edu>
2024-02-06 00:24:41 -0500

    ds

--- ds/ds.hw/ds.hw1/gv2114_hw1_q2.py
+++ ds/ds.hw/ds.hw1/gv2114_hw1_q2.py
@@ -1,2 +1,13 @@
-def shift(lst, k):
-    
+def shift(lst, k, shift = None):
+    for i in range(k):
+        if shift is None:
+            lst.append(lst[0])  # index 0 since value being appended is always
+            # what is at the front
+            lst.pop(0)  # same here for pop always the first index being removed
+        else:
+            lst.insert(0, lst[-1])  # since we want to insert at the front, we do insert
+            # instead of append
+            lst.pop(-1)  # and then remove the last items now
+    return lst
+
+
