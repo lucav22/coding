d8f2918dd5bfba78cc230659012990ab37166c40
lucav22 <luca.villegas@nyu.edu>
2024-02-05 22:31:32 -0500

    ds

---
+++ ds/ds.hw/ds.hw1/gv2114_hw1_q5.py
@@ -0,0 +1,10 @@
+def fibs(n):
+    a, b = 1, 1  # starting point is 1,1 as stated in the hw instructions
+    for i in range(n):
+        yield a
+        a, b = b, a + b  # this switches the place of b with as it moves up and
+        # the new spot of b is taken up by the addition of the current numbers
+
+
+for curr in fibs(8):
+    print(curr)
