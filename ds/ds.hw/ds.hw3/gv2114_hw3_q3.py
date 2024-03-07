2e16318723643a5d8e36c469a0a395201dcb4b9c
lucav22 <luca.villegas@nyu.edu>
2024-03-07 18:46:00 -0500

    ds

--- ds/ds.hw/ds.hw3/gv2114_hw3_q3.py
+++ ds/ds.hw/ds.hw3/gv2114_hw3_q3.py
@@ -1,6 +1,19 @@
 def find_duplicates(lst):
-    counter = []
+    duplicates = []
+    for number in lst:
+        # Since the elements are from 1 to n-1, we adjust by subtracting 1 to use 0-indexed Python lists
+        if lst[abs(number) - 1] < 0:
+            # If the number is already marked, this is a duplicate
+            if abs(number) not in duplicates:
+                duplicates.append(abs(number))
+        else:
+            # Mark the number seen by negating the value at its index
+            lst[abs(number) - 1] *= -1
+    # Restore the list to its original state (optional)
     for i in range(len(lst)):
+        lst[i] = abs(lst[i])
+    return duplicates
+
 
 
 
